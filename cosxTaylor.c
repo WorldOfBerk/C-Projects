3b5749f80a920e7c4555c342ea968bd04a3bff57
WorldOfBerk <99742344+WorldOfBerk@users.noreply.github.com>
2022-06-23 00:20:52 +0300

    Create cosxTaylor.c

---
+++ cosxTaylor.c
@@ -0,0 +1,34 @@
+/* A program that calculate Taylor cosx series*/
+
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.14
+
+int main()
+{
+    float x, formula=1, sum = 0;
+    int factor=1, n = 0, i;
+    
+    printf("\nEnter an angle for cosx > ");
+    scanf("%f", &x);
+    
+    x = x*PI/180;
+    
+    while(formula > 0.00001)
+    {
+        for(i=1;i<2 * n;i++)
+        {
+            factor=factor*i;
+        }
+    
+        formula = (pow(-1,n)*pow(x,2*n))/(factor);
+        n += 1;
+        
+        sum += formula;
+    }
+    
+    printf("%f", sum);
+    
+    return 0;
+}
